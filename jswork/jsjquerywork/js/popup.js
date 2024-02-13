773cc08894787ce16b5726d888223818cb5d5d67
kimminkyu <kmm1604@naver.com>
2024-02-13 17:44:10 +0900

    q

---
+++ jswork/jsjquerywork/js/popup.js
@@ -0,0 +1,9 @@
+function pop1(){
+    window.open("../image/쇼핑몰사진/12.jpg","event","width=500,height=500");
+}
+
+function pop2(){
+   newpopup = window.open("../image/logoImg/a04.png","로고사진","width=400,height=500");
+   setTimeout("newpopup.close()",3000);
+
+}
